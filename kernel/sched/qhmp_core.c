d512b9a37b7535693d3a4f0ecc2a6344474c1e62
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2017-03-21 18:22:49 -0700

    sched: Fix load tracking bug to avoid adding phantom task demand
    When update_task_ravg() is called with the TASK_UPDATE event on a task
    that is not on the runqueue, task demand accounting incorrectly treats
    the time delta as execution time. This can happen when a sleeping
    task is moved to/from colocation groups. This phantom execution time can
    cause unpredictable changes to demand that in turn can result in
    incorrect task placement. Fix the issue by adding special handling of
    TASK_UPDATE in task demand accounting. CPU busy time accounting already
    has all the necessary checks.
    
    Change-Id: Ibb42d83ac353bf2e849055fa3cb5c22e7acd56de
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/qhmp_core.c
+++ kernel/sched/qhmp_core.c
@@ -2121,7 +2121,8 @@ static inline void update_cpu_busy_time(struct task_struct *p, struct rq *rq,
 
 #endif	/* CONFIG_SCHED_FREQ_INPUT */
 
-static int account_busy_for_task_demand(struct task_struct *p, int event)
+static int 
+account_busy_for_task_demand(struct rq *rq, struct task_struct *p, int event)
 {
 	/* No need to bother updating task demand for exiting tasks
 	 * or the idle task. */
@@ -2135,6 +2136,17 @@ static int account_busy_for_task_demand(struct task_struct *p, int event)
 	if (event == TASK_WAKE || (!sched_account_wait_time &&
 			 (event == PICK_NEXT_TASK || event == TASK_MIGRATE)))
 		return 0;
+
+	/*
+	 * TASK_UPDATE can be called on sleeping task, when its moved between
+	 * related groups
+	 */
+	if (event == TASK_UPDATE) {
+		if (rq->curr == p)
+			return 1;
+
+		return p->on_rq ? sched_account_wait_time : 0;
+	}
 
 	return 1;
 }
@@ -2271,7 +2283,7 @@ static void update_task_demand(struct task_struct *p, struct rq *rq,
 	u32 window_size = sched_ravg_window;
 
 	new_window = mark_start < window_start;
-	if (!account_busy_for_task_demand(p, event)) {
+	if (!account_busy_for_task_demand(rq, p, event)) {
 		if (new_window)
 			/* If the time accounted isn't being accounted as
 			 * busy time, and a new window started, only the
