8a8ae8a88cd1087c297e3ecaea484e293a328644
Nich <nctrenco@gmail.com>
2019-04-14 18:23:14 +0000

    ASoC: msm8952: Restore old routing CPU DAI names
    We need to bring back the deprecated DAIs because the new one didn't
    work for our device.

--- techpack/audio/asoc/msm8952-dai-links.c
+++ techpack/audio/asoc/msm8952-dai-links.c
@@ -361,7 +361,7 @@ static struct snd_soc_dai_link msm8952_common_fe_dai[] = {
 	{/* hw:x,2 */
 		.name = "Circuit-Switch Voice",
 		.stream_name = "CS-Voice",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "CS-VOICE",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_capture = 1,
@@ -534,7 +534,7 @@ static struct snd_soc_dai_link msm8952_common_fe_dai[] = {
 	{/* hw:x,13 */
 		.name = "Voice2",
 		.stream_name = "Voice2",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "Voice2",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_capture = 1,
@@ -569,7 +569,7 @@ static struct snd_soc_dai_link msm8952_common_fe_dai[] = {
 	{ /* hw:x,15 */
 		.name = "VoLTE",
 		.stream_name = "VoLTE",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "VoLTE",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_capture = 1,
@@ -587,7 +587,7 @@ static struct snd_soc_dai_link msm8952_common_fe_dai[] = {
 	{ /* hw:x,16 */
 		.name = "VoWLAN",
 		.stream_name = "VoWLAN",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "VoWLAN",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_capture = 1,
--- techpack/audio/asoc/msm8952.c
+++ techpack/audio/asoc/msm8952.c
@@ -1716,7 +1716,7 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 	{/* hw:x,2 */
 		.name = "Circuit-Switch Voice",
 		.stream_name = "CS-Voice",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "CS-VOICE",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_playback = 1,
@@ -1891,7 +1891,7 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 	{/* hw:x,13 */
 		.name = "Voice2",
 		.stream_name = "Voice2",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "Voice2",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_playback = 1,
@@ -1926,7 +1926,7 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 	{ /* hw:x,15 */
 		.name = "VoLTE",
 		.stream_name = "VoLTE",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "VoLTE",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_playback = 1,
@@ -1944,7 +1944,7 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 	{ /* hw:x,16 */
 		.name = "VoWLAN",
 		.stream_name = "VoWLAN",
-		.cpu_dai_name   = "VoiceMMode1",
+		.cpu_dai_name   = "VoWLAN",
 		.platform_name  = "msm-pcm-voice",
 		.dynamic = 1,
 		.dpcm_playback = 1,
