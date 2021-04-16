a41924ad66c1c539afb9ab41e68be6c86a622474
BeYkeRYkt <beykerykt@gmail.com>
2021-04-16 18:33:53 +0900

    input: touchscreen: ilitek: Handle more framebuffer hints
    * Resume touchscreen functions during doze mode to allow the user
      to interact with the screen.
    
    Change-Id: I71d14b33a452d76a9bd122b7479aea89a894205c

--- drivers/input/touchscreen/ilitek_2120/ilitek_lim.c
+++ drivers/input/touchscreen/ilitek_2120/ilitek_lim.c
@@ -1894,7 +1894,9 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 			ts && ts->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK) {
+		if (*blank == FB_BLANK_UNBLANK
+				|| *blank == FB_BLANK_NORMAL
+				|| *blank == FB_BLANK_VSYNC_SUSPEND) {
 			printk("fb_notifier_callback ilitek_ts_resume\n");
 			ilitek_ts_resume(ts);
 		}
